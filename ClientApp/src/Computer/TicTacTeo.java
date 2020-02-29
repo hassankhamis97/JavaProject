0ab2745a3bdddd77f9a051d5db8b8ca7448e8e60
iAhmedAwad <ahmed.mmawad@hotmail.com>
2020-02-29 16:53:57 +0200

    AI modification
    1- static class Move is deleted and we are now dealing with an object of GamePositionAIModel
    2- PlayerVSComputer and TicTacTeo are now integrated (PlayerVSComputer sends the current ArrayList to GamePositionAIModel and receives index to play)

--- ClientApp/src/Computer/TicTacTeo.java
+++ ClientApp/src/Computer/TicTacTeo.java
@@ -13,10 +13,10 @@ import java.util.Scanner;
  * @author Salah Kamal
  */
 public class TicTacTeo {
+    
 
-    private static Move findRandomMove(ArrayList<Move> b) {
-        Move v = null;
-        ArrayList<Move> remainMoves = new ArrayList<>();
+     static GamePositionAIModel findRandomMove(ArrayList<GamePositionAIModel> b) {
+        ArrayList<GamePositionAIModel> remainMoves = new ArrayList<>();
         for(int i = 0; i<9; i++){
             if(b.get(i).PositionTxt.isEmpty()){
                 remainMoves.add(b.get(i));
@@ -26,7 +26,7 @@ public class TicTacTeo {
         int i = (int)(Math.random() * remainMoves.size());
         return remainMoves.get(i);
     }
-
+/*
     static class Move {
 
         public String PositionTxt;
@@ -41,8 +41,8 @@ public class TicTacTeo {
 
         }
     }
-
-    static Boolean isMovesLeft(ArrayList<Move> board) {
+*/
+    static Boolean isMovesLeft(ArrayList<GamePositionAIModel> board) {
         for (int i = 0; i < 9; i++) {
             if (board.get(i).PositionTxt.equals("")) {
                 return true;
@@ -51,7 +51,7 @@ public class TicTacTeo {
         return false;
     }
 
-    static int evaluate(ArrayList<Move> b) {
+    static int evaluate(ArrayList<GamePositionAIModel> b) {
 
         // Checking for Rows for X or O victory. 
         if (b.get(0).PositionTxt.equalsIgnoreCase(b.get(1).PositionTxt)
@@ -134,7 +134,7 @@ public class TicTacTeo {
         return 0;
     }
 
-    static int minimax(ArrayList<Move> board,
+    static int minimax(ArrayList<GamePositionAIModel> board,
             int depth, Boolean isMax) {
         int score = evaluate(board);
 
@@ -199,9 +199,11 @@ public class TicTacTeo {
     // This will return the best possible 
     // move for the player 
 
-    static Move findBestMove(ArrayList<Move> board) {
+    static GamePositionAIModel findBestMove(ArrayList<GamePositionAIModel> board) {
+        
+        System.out.println("I am inside best move");
         int bestVal = -1000;
-        Move bestMove = new Move();
+        GamePositionAIModel bestMove = new GamePositionAIModel();
         bestMove.PositionIndex = -1;
         bestMove.PositionTxt = "";
 
@@ -238,30 +240,30 @@ public class TicTacTeo {
     }
 
     public static void main(String[] args) {
-        ArrayList<Move> b = new ArrayList<>();
-        b.add(new Move("o", 0));
-        b.add(new Move("", 1));
-        b.add(new Move("", 2));
-        b.add(new Move("", 3));
-        b.add(new Move("", 4));
-        b.add(new Move("", 5));
-        b.add(new Move("", 6));
-        b.add(new Move("", 7));
-        b.add(new Move("", 8));
+        ArrayList<GamePositionAIModel> b = new ArrayList<>();
+        b.add(new GamePositionAIModel("o", 0));
+        b.add(new GamePositionAIModel("o", 1));
+        b.add(new GamePositionAIModel("", 2));
+        b.add(new GamePositionAIModel("x", 3));
+        b.add(new GamePositionAIModel("", 4));
+        b.add(new GamePositionAIModel("", 5));
+        b.add(new GamePositionAIModel("x", 6));
+        b.add(new GamePositionAIModel("", 7));
+        b.add(new GamePositionAIModel("o", 8));
         Scanner scan = new Scanner(System.in);
         boolean f = true;
         System.out.print("Enter 1 for Easy ,3 for Hard:");
         int x = scan.nextInt();
         if (x == 3) {
-            Move bestMove = findBestMove(b);
+            GamePositionAIModel bestMove = findBestMove(b);
             System.out.println("index= " + bestMove.PositionIndex);
             System.out.println("txt= " + bestMove.PositionTxt);
         } else if (x == 1) {
-            Move bestMove = findRandomMove(b);
+            GamePositionAIModel bestMove = findRandomMove(b);
             System.out.println("index= " + bestMove.PositionIndex);
             System.out.println("txt= " + bestMove.PositionTxt);
         } else if (x == 2){
-            Move bestMove ;
+            GamePositionAIModel bestMove ;
             if(f){
                 bestMove = findBestMove(b);
                 f = !f;
