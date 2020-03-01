d1a9175a79424f85de966ff90811e906af22fcfd
iAhmedAwad <ahmed.mmawad@hotmail.com>
2020-03-02 00:54:33 +0200

    Update PlayerVSComputerv2.java
    Added winner/loser conditions / checking

--- ClientApp/src/Computer/PlayerVSComputerv2.java
+++ ClientApp/src/Computer/PlayerVSComputerv2.java
@@ -10,7 +10,11 @@ import Game.GameUINew;
 import Pojos.Moves;
 
 import Stack.NavigationStack;
+import clientapp.GameStart;
+import clientapp.Loser;
+import clientapp.Main;
 import clientapp.SharedData;
+import clientapp.Winner;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -59,10 +63,11 @@ public class PlayerVSComputerv2 extends GameUINew {
         button1.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-               // button1.setText("X");
+                button1.setText("X");
                 imageBtn1.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button1.setDisable(true);
-                
+
+                checkX();
 
                 for (GamePositionAIModel item : positionLst) {
 
@@ -88,10 +93,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button2.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-                //button2.setText("X");
+                button2.setText("X");
                 imageBtn2.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button2.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 1) {
@@ -115,10 +120,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button3.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-//                button3.setText("X");
+                button3.setText("X");
                 imageBtn3.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button3.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 2) {
@@ -143,10 +148,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button4.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-//                button3.setText("X");
+                button4.setText("X");
                 imageBtn4.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button4.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 3) {
@@ -170,10 +175,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button5.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-//                button4.setText("X");
+                button5.setText("X");
                 imageBtn5.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button5.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 4) {
@@ -197,10 +202,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button6.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-//                button5.setText("X");
+                button6.setText("X");
                 imageBtn6.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button6.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 5) {
@@ -224,10 +229,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button7.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-//                button6.setText("X");
+                button7.setText("X");
                 imageBtn7.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button7.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 6) {
@@ -251,10 +256,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button8.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-               // button7.setText("X");
+                button8.setText("X");
                 imageBtn8.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button8.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 7) {
@@ -278,10 +283,10 @@ public class PlayerVSComputerv2 extends GameUINew {
         button9.setOnAction(t -> {
             if (!isWaitAIResponse) {
                 this.isWaitAIResponse = true;
-//                button8.setText("X");
+                button9.setText("X");
                 imageBtn9.setImage(new Image(GameUI.class.getResource("/Game/x.png").toExternalForm()));
                 button9.setDisable(true);
-
+                checkX();
                 for (GamePositionAIModel item : positionLst) {
 
                     if (item.PositionIndex == 8) {
@@ -331,44 +336,115 @@ public class PlayerVSComputerv2 extends GameUINew {
         return index;
     }
 
+    private void checkX() {
+        if (checkWinningLetter()) {
+
+            new Winner();
+            Main.showNewScene(SharedData.nsList.get(SharedData.nsList.size() - 2).root);
+        }
+
+    }
+
+    private void checkO() {
+        if (checkWinningLetter()) {
+
+            new Loser();
+            Main.showNewScene(SharedData.nsList.get(SharedData.nsList.size() - 2).root);
+
+        }
+
+    }
+
+    private boolean checkWinningLetter() {
+
+        String t1 = button1.getText();
+        String t2 = button2.getText();
+        String t3 = button3.getText();
+        String t4 = button4.getText();
+        String t5 = button5.getText();
+        String t6 = button6.getText();
+        String t7 = button7.getText();
+        String t8 = button8.getText();
+        String t9 = button9.getText();
+
+        if ( //Horizontal
+                ((t1.equals(t2) && t1.equals(t3) && ((t1.equalsIgnoreCase("x") || (t1.equalsIgnoreCase("o")))) && !t1.isEmpty())
+                || (t4.equals(t5) && t4.equals(t6) && ((t4.equalsIgnoreCase("x") || (t4.equalsIgnoreCase("o")))) && !t4.isEmpty())
+                || (t7.equals(t8) && t7.equals(t9)) && ((t4.equalsIgnoreCase("x") || (t4.equalsIgnoreCase("o")))) && !t7.isEmpty())
+                || //Vertical
+                ((t1.equals(t4) && t1.equals(t7) && ((t1.equalsIgnoreCase("x") || (t1.equalsIgnoreCase("o")))) && !t1.isEmpty())
+                || (t2.equals(t5) && t2.equals(t8) && ((t2.equalsIgnoreCase("x") || (t2.equalsIgnoreCase("o")))) && !t2.isEmpty())
+                || (t7.equals(t8) && t7.equals(t9)) && ((t7.equalsIgnoreCase("x") || (t7.equalsIgnoreCase("o")))) && !t7.isEmpty())
+                || //Diagonals
+                ((t1.equals(t5) && t1.equals(t9) && ((t1.equalsIgnoreCase("x") || (t1.equalsIgnoreCase("o")))) && !t1.isEmpty())
+                || (t3.equals(t5) && t3.equals(t7)) && ((t3.equalsIgnoreCase("x") || (t3.equalsIgnoreCase("o")))) && !t3.isEmpty())) {
+
+            /*
+             new Winner();
+             Main.showNewScene(SharedData.nsList.get(SharedData.nsList.size() - 2).root);
+             */
+            return true;
+        }
+
+        return false;
+
+    }
+
     private void setPositionTxtByComputer(int computerSelectedIndex) {
         switch (computerSelectedIndex) {
             case 0:
-               // button1.setText("O");
+                button1.setText("O");
                 imageBtn1.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button1.setDisable(true);
+                checkO();
                 break;
             case 1:
+                button2.setText("O");
                 imageBtn2.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button2.setDisable(true);
+                checkO();
                 break;
             case 2:
+                button3.setText("O");
                 imageBtn3.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button3.setDisable(true);
+                checkO();
                 break;
             case 3:
+                button4.setText("O");
                 imageBtn4.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button4.setDisable(true);
+                checkO();
                 break;
             case 4:
+                button5.setText("O");
                 imageBtn5.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button5.setDisable(true);
+                checkO();
                 break;
             case 5:
+                button6.setText("O");
                 imageBtn6.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button6.setDisable(true);
+                checkO();
                 break;
             case 6:
+                button7.setText("O");
                 imageBtn7.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button7.setDisable(true);
+                checkO();
                 break;
             case 7:
+                button8.setText("O");
                 imageBtn8.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button8.setDisable(true);
+                checkO();
                 break;
             case 8:
+                button9.setText("O");
                 imageBtn9.setImage(new Image(GameUI.class.getResource("/Game/o.png").toExternalForm()));
                 button9.setDisable(true);
+                checkO();
                 break;
         }
     }
